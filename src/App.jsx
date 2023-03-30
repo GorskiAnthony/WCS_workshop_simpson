162ce6ad7db8896ea3b38ae7c4d232a55e75cfb7
Anthony Gorski <[anthony.gorski@wildcodeschool.com](mailto:anthony.gorski@wildcodeschool.com)>
2023-03-30 10:27:08 +0200

    refacto bart & homer

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,28 @@
 import Avatar from "./components/Avatar";
 
 function App() {
+	const bart = {
+		image: "https://www.stickees.com/files/cartoon/the-simpsons/2239-bart-simpson-.png",
+		firstName: "Bart",
+		lastName: "Simpson",
+	};
+
+	const homer = {
+		image: "https://www.stickees.com/files/cartoon/the-simpsons/2246-homer-simpson-donut-2.png",
+		firstName: "Homer",
+		lastName: "Simpson",
+	};
 	return (
 		<div className="App">
 			<Avatar
-				image="https://www.stickees.com/files/cartoon/the-simpsons/2239-bart-simpson-.png"
-				firstName="Bart"
-				lastName="Simpson"
+				image={bart.image}
+				firstName={bart.firstName}
+				lastName={bart.lastName}
 			/>
 			<Avatar
-				image="https://www.stickees.com/files/cartoon/the-simpsons/2246-homer-simpson-donut-2.png"
-				firstName="Homer"
-				lastName="Simpson"
+				image={homer.image}
+				firstName={homer.firstName}
+				lastName={homer.lastName}
 			/>
 		</div>
 	);
